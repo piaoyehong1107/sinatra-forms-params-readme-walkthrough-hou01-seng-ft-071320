b788aa89eeded6a6813d4ab94b868b008caa17f1
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-05 22:48:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,5 +7,10 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
+  post '/food' do
+    @name=params[:name]
+    @favorite_food=params[:favorite_food]
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
+  end
 
 end
